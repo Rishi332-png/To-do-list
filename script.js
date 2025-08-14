1ce970e9788ddaf7ccfd11ab06217129fce73290
Rishi332-png <rishibhardwaj802@gmail.com>
2025-08-14 10:14:12 +0530

    Add files via upload
    To-Do List App
    
    A simple and interactive to-do list web application built with HTML, CSS, and JavaScript. This project allows users to manage daily tasks efficiently with an intuitive interface.
    
    🚀 Features
    
    * Add Tasks — Input new tasks and add them to the list.
    
    * Mark as Completed — Click a task to toggle a completed style.
    
    * Delete Tasks — Remove tasks instantly with a delete button.
    
    * Responsive Design — Works on desktop and mobile devices.
    
    🛠️ Tech Stack
    
    * HTML5 — Page structure.
    
    * CSS3 — Styling and layout.
    
    * JavaScript (Vanilla) — DOM manipulation and event handling.
    
    
    📚 Learning Outcomes
    
    Practical experience with DOM selection and manipulation.
    
    Adding and removing elements dynamically.
    
    Using CSS classes for conditional styling.

---
+++ script.js
@@ -0,0 +1,38 @@
+document.querySelector('#push').onclick =function input(){
+     if(document.querySelector('#newtask input').value.length ==0){
+        alert("Please Enter a Task")
+     }
+     else{
+        document.querySelector('#tasks').innerHTML +=`
+         <div class="task">
+         <span id="taskname">
+            ${document.querySelector('#newtask input').value}
+         </span>
+          <button class="delete">
+             <i class="fa-regular fa-trash"></i>
+          </button>
+         </div>`;
+        
+
+        //Remove text when  delete button press
+         var current_task = document.querySelectorAll(".delete");
+         for(var i=0 ; i<current_task.length ; i++){
+             current_task[i].onclick = function(){
+                this.parentNode.remove();
+             }
+         }
+         
+         //Crossing the task on clicking on it
+          var tasks = document.querySelectorAll('.task');
+        for(var i=0 ; i<tasks.length ; i++){
+            tasks[i].onclick =function(){
+                this.classList.toggle('completed'); //this line means when .completed class not there add it when there remove it
+            }
+        }
+        
+       
+}
+  //Clear the input section after add button press
+ document.querySelector('#newtask input');
+        value ="";
+}
